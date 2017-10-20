f3e21d7f7aaeb9540aed2e0eecf0673b96306955
michaelygzhang <g.zhangyg@gmail.com>
2017-10-20 08:50:06 +0800

    init lemicp

---
+++ src/main/java/com/spider/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.spider.service;
+
+/**
+ * Created by michael on 2017/10/20.
+ */
+public interface IUserService {
+}
---
+++ src/main/java/com/spider/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.spider.service.impl;
+
+/**
+ * Created by michael on 2017/10/20.
+ */
+public class UserServiceImpl {
+}
