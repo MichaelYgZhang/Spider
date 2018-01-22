8a7e51bdf6092ca38b2d914f50d614ffd6b200e5
michaelygzhang <g.zhangyg@gmail.com>
2018-01-22 08:44:18 +0800

    reload

--- src/main/java/com/spider/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.spider.service;
-
-/**
- * Created by michael on 2017/10/20.
- */
-public interface IUserService {
-}
--- src/main/java/com/spider/service/impl/UserServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.spider.service.impl;
-
-/**
- * Created by michael on 2017/10/20.
- */
-public class UserServiceImpl {
-}
